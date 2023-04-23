e3dd8dba65f799b4647ee0fca54190272920bdf2
Alexandre Machado <alexandre_fmachado@hotmail.com>
2023-04-23 17:11:33 -0300

    add more analysis to py app

--- py/main.py
+++ py/main.py
@@ -3,24 +3,34 @@ import time
 import winsound
 import requests
 
-output = TA_Handler(symbol='BTCUSDT',
+output15m = TA_Handler(symbol='BTCUSDT',
                     screener='Crypto',
                     exchange='Binance',
                     interval=Interval.INTERVAL_15_MINUTES)
+
+output1w = TA_Handler(symbol='BTCUSDT',
+                    screener='Crypto',
+                    exchange='Binance',
+                    interval=Interval.INTERVAL_1_WEEK)
 
 url = 'http://localhost:3003/'
 
 previousRSI = 0
 currentRSI = 0
+previousEMA20 = 0
+currentEMA20 = 0
+previousEMA50 = 0
+currentEMA50 = 0
+currentStochRSI = 0
+
 print('Application started.')
 
-
 def hour():
-    formatted = f"{time.localtime().tm_hour}:{time.localtime().tm_min}:{time.localtime().tm_sec}"
+    formatted = f'{time.localtime().tm_hour}:{time.localtime().tm_min}:{time.localtime().tm_sec}'
     print(formatted)
 
 
-def neworder(operation):
+def newOrder(operation):
     response = requests.get(url + operation)
     data = response.json()
     print(data)
@@ -28,10 +38,95 @@ def neworder(operation):
     hour()
 
 
-def currentandpreviousRSI(currentRSI, previousRSI):
-    print("Current RSI:", currentRSI)
-    print("Previous RSI:", previousRSI)
+def statusRSI(currentRSI, previousRSI):
+    if previousRSI <= 30 and currentRSI > 30:
+        print('🟢 RSI buy signal', currentRSI)
+        return True
+    if previousRSI >= 70 and currentRSI < 70:
+        print('🔴 RSI sell signal', currentRSI)
+        return False
+    else:
+        print('🟡 RSI neutral:', currentRSI)
+        return currentRSI
 
+
+def statusStochRSI(currentStochRSI):
+    if currentStochRSI < 20:
+        print('🟢 Stoch RSI buy signal', currentStochRSI)
+        return True
+    if currentStochRSI > 80:
+        print('🔴 Stoch RSI sell signal', currentStochRSI)
+        return False
+    else:
+        print('🟡 Stoch RSI neutral:', currentStochRSI)
+        return currentStochRSI
+    
+    
+def statusMACD():
+    macd = output15m.get_analysis().indicators['MACD.macd']
+    signal = output15m.get_analysis().indicators['MACD.signal']
+    if (macd < 0 and signal < 0) and signal < macd:
+        print('🟢 Signal lower than MACD above 0', '\n\tMACD:', macd, '\n\tSignal:', signal)
+        return True
+    if (macd > 0 and signal > 0) and signal > macd:
+        print('🔴 Signal higher than MACD above 0', '\n\tMACD:', macd, '\n\tSignal:', signal)
+        return False
+    else:
+        print('🟡 MACD neutral:', '\n\tMACD:', macd, '\n\tSignal:', signal)
+        return macd
+
+
+def isPointedUpEMA20(currentEMA20, previousEMA20):
+    if currentEMA20 > previousEMA20:
+        print('🟢 EMA 20 is pointed up')
+        return True
+    else:
+        print('🔴 EMA 20 is pointed down')
+        return False
+
+    
+def isPointedUpEma50(currentEMA50, previousEMA50):
+    if currentEMA50 > previousEMA50:
+        print('🟢 EMA 50 is pointed up')
+        return True
+    else:
+        print('🔴 EMA 50 is pointed down')
+        return False
+    
+
+def isEMA20AboveEMA50(currentEMA20, currentEMA50):
+    if currentEMA20 > currentEMA50:
+        print('🟢 EMA 20 is above EMA 50')
+        return True
+    else:
+        print('🔴 EMA 20 is below EMA 50')
+        return False
+            
+
+def isPriceAboveWeeklyEMA8():
+    ema5 = output1w.get_analysis().indicators['EMA5']
+    ema10 = output1w.get_analysis().indicators['EMA10']
+    price = output1w.get_analysis().indicators['close']
+    ema8 = (ema10+ema5) / 2
+    if price > ema8:
+        print('🟢 Price is above weekly EMA 8')
+        return True
+    else:
+        print('🔴 Price is below weekly EMA 8')
+        return False
+    
+    
+def isPriceAboveEMA200():
+    ema200 = output15m.get_analysis().indicators['EMA200']
+    price = output15m.get_analysis().indicators['close']
+    if price > ema200:
+        print('🟢 Price is above EMA 200')
+        return True
+    else:
+        print('🔴 Price is below EMA 200')
+        return False
+
+## todo: criar estratégias: media 8 semanal, medias 20+50+estocastico, media 200 + macd
 
 while 1 == 1:
     time.sleep(1)
@@ -39,28 +134,46 @@ while 1 == 1:
     second = time.localtime().tm_sec
     condition = (minute == 0 and second == 0) or (minute == 15 and second == 0) or \
                 (minute == 30 and second == 0) or (minute == 45 and second == 0)
+
     if condition:
-        rsi = output.get_analysis().indicators['RSI']
+        hour()
+        rsi = output15m.get_analysis().indicators['RSI']
+        stochRSI = output15m.get_analysis().indicators['Stoch.RSI.K']
+        ema20 = output15m.get_analysis().indicators['EMA20']
+        ema50 = output15m.get_analysis().indicators['EMA50']
+        
         if previousRSI == 0:
             previousRSI = rsi
         else:
             previousRSI = currentRSI
         currentRSI = rsi
-
-        if previousRSI >= 70 and currentRSI < 70:
-            print("RSI>=70")
-            neworder('sell')
-            print("RSI:", rsi)
-            print('--------------------------------------')
-        if previousRSI <= 30 and currentRSI > 30:
-            print("RSI<=30")
-            neworder('buy')
-            print("RSI:", rsi)
-            print('--------------------------------------')
+        
+        if previousEMA20 == 0:
+            previousEMA20 = ema20
         else:
-            print("RSI ok")
-            print("RSI:", rsi)
-            hour()
-            print('--------------------------------------')
-        currentandpreviousRSI(currentRSI, previousRSI)
-        print("===================================================")
+            previousEMA20 = currentEMA20
+        currentEMA20 = ema20
+        
+        if previousEMA50 == 0:
+            previousEMA50 = ema50
+        else:
+            previousEMA50 = currentEMA50
+        currentEMA50 = ema50
+        
+        evalRSI = statusRSI(currentRSI, previousRSI)
+
+        if evalRSI == False:
+            newOrder('sell')
+        if evalRSI == True:
+            newOrder('buy')
+
+        isPriceAboveWeeklyEMA8()
+        isPriceAboveEMA200()
+        statusStochRSI(stochRSI)
+        isPointedUpEMA20(currentEMA20, previousEMA20)
+        isPointedUpEma50(currentEMA50, previousEMA50)
+        isEMA20AboveEMA50(currentEMA20, currentEMA50)
+        statusMACD()
+        
+
+        print('===================================================')
